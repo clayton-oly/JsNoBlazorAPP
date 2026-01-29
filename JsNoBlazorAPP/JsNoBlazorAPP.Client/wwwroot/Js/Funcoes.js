75bc0e8fa292584d434ea42b78a5d5ce89bf23a3
Clayton Oliveira <clayton.will08@gmail.com>
2026-01-29 20:19:28 -0300

    Ajuste

---
+++ JsNoBlazorAPP/JsNoBlazorAPP.Client/wwwroot/Js/Funcoes.js
@@ -0,0 +1,14 @@
+﻿//função exemploFuncaoSemRetorno
+window.exemploFuncaoSemRetorno = function () {
+    alert("Hello World");
+    console.log("Hello World");
+}
+
+//função exemploFuncaoComRetorno
+window.exemploFuncaoComRetorno = function
+    (nome) {
+    saudacao = "Hello  " + nome;
+    alert("Hello World");
+    console.log(saudacao);
+    return saudacao;
+}
